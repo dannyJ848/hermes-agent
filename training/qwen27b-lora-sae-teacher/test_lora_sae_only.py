9a0c313f6b0f9e2371906a08a41ae9fdcaf67e2b
dannyJ848 <61113300+dannyJ848@users.noreply.github.com>
2026-05-04 03:33:58 -0500

    test: disable teacher/SAE/curriculum - test CE-only LoRA first

--- training/qwen27b-lora-sae-teacher/test_lora_sae_only.py
+++ training/qwen27b-lora-sae-teacher/test_lora_sae_only.py
@@ -6,9 +6,9 @@ sys.path.insert(0, '/data/SpecForge/custom_dflash')
 from train_lora_sae_teacher_v1 import TrainConfig, train
 
 config = TrainConfig()
-config.use_teacher = True  # Enable teacher distillation
-config.use_sae = True
-config.use_curriculum = True
+config.use_teacher = False  # Disable teacher - causes dtype issues with quantization
+config.use_sae = False  # Disable SAE - causes dtype issues with quantization
+config.use_curriculum = False
 config.max_steps = 100  # Just 100 steps for testing
 config.batch_size = 1
 config.grad_accum_steps = 4
